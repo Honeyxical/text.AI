9f148124215927dd7822b5d3e094e1e41cef92dd
ilyabenikov <vatnaya1997@gmail.com>
2021-10-14 21:18:43 +0300

    Added basic methods.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+double antiPlagiarism (string text, string fragment); // основной метод
+
+string deleteUnions(string text); // удалить связи
+
+string deleteSeparators(string text); // удалить разделители
+
+int getShingleHash(string text); // получение шингл хэша
+
+int countWordsText(string text); // количество слов в тексте
+
 int main() {
     cout << "Ok";
     return 0;
