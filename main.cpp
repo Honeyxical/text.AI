0f3300207786eb229062d65e259171bb5ef60716
ilyabenikov <vatnaya1997@gmail.com>
2021-10-14 21:19:23 +0300

    Added basic methods.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,6 @@ int getShingleHash(string text); // получение шингл хэша
 int countWordsText(string text); // количество слов в тексте
 
 int main() {
-    cout << "Ok";
+    cout << "O";
     return 0;
 }
