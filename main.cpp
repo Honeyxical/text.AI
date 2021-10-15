c17b43d2d43911712e101542fa4b9cd55e068832
ilyabenikov <vatnaya1997@gmail.com>
2021-10-16 01:17:04 +0300

    Added method "antiPlagiarism". Some fix.

--- main.cpp
+++ main.cpp
@@ -4,23 +4,79 @@
 
 using namespace std;
 
-double antiPlagiarism(string text, string fragment); // основной метод
-string deleteUnions(string text); // удалить союз
-string deleteSeparators(string text); // удалить разделители +
-string getShingleHash(string shingle); // получение шингл хэша +
-string getShingle(string text); // +
-string deleteOneItemFromShingle(string text); // +
-string toLowerCase(string text); // +
-int countWordsText(string text); // количество слов в тексте +
-bool isEqual(string shingleFromUserText, string shingleFromTextDB);
+double antiPlagiarism(string text, string fragment);
+
+string deleteUnions(string text);
+
+string deleteSeparators(string text);
+
+string getShingleHash(string shingle);
+
+string getShingle(string text);
+
+string deleteOneItemFromShingle(string text);
+
+string toLowerCase(string text);
+
+int countWords(string text);
 
 int main() {
-    string testText = "   Lorem Ipsum comes from sections h u i  gg g 'g!g  1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC.***";
+    string text = "Lorem ipsum dolor so sit amet, consectetur adipiscing elit. Morbi id lorem eu purus vehicula pellentesque. "
+                  "Suspendisse enim augue, dictum nec eros nec, rutrum varius velit. Nam vulputate egestas enim pretium congue. "
+                  "Donec a tristique turpis, vestibulum varius tellus. Quisque imperdiet semper fringilla. Proin in vehicula purus. "
+                  "Nulla vehicula ut sem eget luctus. Aliquam porta ullamcorper efficitur. Etiam maximus orci ac augue egestas, sit amet commodo magna egestas. "
+                  "Duis sagittis auctor felis sed iaculis. Donec vulputate non urna sed finibus. Quisque aliquam convallis ante non mattis. "
+                  "Nulla eu libero convallis, vulputate magna id, interdum mi. Etiam sit amet efficitur arcu, ac malesuada est. "
+                  "Vivamus facilisis ex quis erat auctor, sit amet dictum metus interdum.";
+    string fragment = "Lorem ipsum dolor so sit amet, consectetur adipiscing elit. Donec et tempor odio, vitae condimentum risus. "
+                      "Fusce posuere, quam venenatis placerat porttitor, augue nulla porta magna, quis lacinia dolor enim non metus. "
+                      "Praesent elementum enim id rutrum volutpat. Duis maximus vitae nisl eu efficitur. Ut dapibus lobortis augue quis porttitor. "
+                      "Sed magna risus, varius fringilla ipsum vel, rhoncus bibendum lectus. Donec ut justo pellentesque, fringilla leo maximus, pharetra ipsum. "
+                      "Maecenas convallis diam et massa pulvinar auctor. Nam maximus nunc ipsum, sed semper lorem imperdiet eget. "
+                      "Donec nisi quam, mattis in maximus a, viverra a libero. Quisque vitae velit eget diam sagittis varius. "
+                      "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. "
+                      "Proin vitae libero gravida, vulputate lectus vulputate, dapibus augue. Aliquam tincidunt sed purus mollis pharetra. "
+                      "Sed nisi nulla, sollicitudin id urna non, dictum scelerisque lorem. Vivamus ut luctus ante.";
+
+    cout << antiPlagiarism(text, fragment);
+
     return 0;
 }
 
-bool isEqual(string shingleFromUserText, string shingleFromTextDB) {
-    return shingleFromUserText == shingleFromTextDB;
+double antiPlagiarism(string text, string fragment) {
+    if (fragment == "" || text == "") {
+        cout << "Error!";
+        return -1;
+    }
+    double countEquals = 0;
+    double countFragmentShingles = 0;
+    double countTextShingles = 0;
+    string bufferText;
+    string shingleFragment;
+    string shingleFragmentHash;
+    string shingleText;
+    string shingleTextHash;
+
+    text = deleteUnions(toLowerCase(deleteSeparators(text)));
+    cout << text << endl;
+    fragment = deleteUnions(toLowerCase(deleteSeparators(fragment)));
+    cout << fragment;
+
+    for (int i = 0; countWords(fragment) >= 3; i++) {
+        bufferText = text;
+        shingleFragmentHash = getShingleHash(getShingle(fragment));
+        countFragmentShingles++;
+        fragment = deleteOneItemFromShingle(fragment);
+        for (int j = 0; countWords(bufferText) >= 3; j++) {
+            shingleTextHash = getShingleHash(getShingle(bufferText));
+            countTextShingles++;
+            bufferText = deleteOneItemFromShingle(bufferText);
+            if (shingleFragmentHash == shingleTextHash) {
+                countEquals++;
+            }
+        }
+    }
+    return (countEquals / countFragmentShingles) * 100.0;
 }
 
 string deleteOneItemFromShingle(string text) {
@@ -44,16 +100,18 @@ string getShingle(string text) {
     string shingle;
     int wordsCount = 0;
 
-    for (int i = 0; text[i] != '\0'; i++) {
-        if (text[i] != ' ') {
-            while (text[i] != ' ') {
-                shingle += text[i];
-                i++;
-            }
-            shingle += " ";
-            wordsCount++;
-            if (wordsCount == 3) {
-                return shingle;
+    if (countWords(text) >= 3) {
+        for (int i = 0; text[i] != '\0'; i++) {
+            if (text[i] != ' ' && text[i] != '\0') {
+                while (text[i] != ' ' && text[i] != '\0') {
+                    shingle += text[i];
+                    i++;
+                }
+                shingle += " ";
+                wordsCount++;
+                if (wordsCount == 3) {
+                    return shingle;
+                }
             }
         }
     }
@@ -67,7 +125,7 @@ string deleteUnions(string text) {
     int position;
 
     for (int i = 0; text[i] != '\0'; i++) {
-        if (text[i] != ' ') {
+        if (text[i] != ' ' && text[i] != '\0') {
             position = i;
             while (text[i] != ' ' && text[i] != '\0') {
                 wordText += text[i];
@@ -75,8 +133,8 @@ string deleteUnions(string text) {
             }
         }
         for (int j = 0; stringUnions[j] != '\0'; ++j) {
-            if (stringUnions[j] != ' ') {
-                while (stringUnions[j] != ' ') {
+            if (stringUnions[j] != ' ' && stringUnions[j] != '\0') {
+                while (stringUnions[j] != ' ' && stringUnions[j] != '\0') {
                     wordUnion += stringUnions[j];
                     j++;
                 }
@@ -94,11 +152,11 @@ string deleteUnions(string text) {
     return text;
 }
 
-int countWordsText(string text) {
+int countWords(string text) {
     int countWords = 0;
     for (int i = 0; i < text.length(); ++i) {
-        if (text[i] != ' ') {
-            while (text[i] != ' ') {
+        if (text[i] != ' ' && text[i] != '\0') {
+            while (text[i] != ' ' && text[i] != '\0') {
                 i++;
             }
             countWords++;
